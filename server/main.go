ae1551cc6a6bc7d976d45a86e611ecd520ff8d6e
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-02-20 13:35:46 +0100

    add cors

--- server/main.go
+++ server/main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 
 	// Middleware (Logger)
 	app.Use(logger.New())
